3c8b2cc2407b0a777d28520038305c0c3a7ebec4
cristianoenke <59913662+cristianoenke@users.noreply.github.com>
2024-11-10 16:04:38 -0300

    Update app.py

--- app_v1.3_Render/app.py
+++ app_v1.3_Render/app.py
@@ -252,4 +252,4 @@ def excluir_exercicio():
     return render_template('excluir_exercicio.html', exercicios=exercicios)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5432)))
